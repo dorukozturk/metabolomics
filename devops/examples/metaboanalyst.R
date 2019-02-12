c5bbb3c15efbf5ab8962cdf665282128981c53a0
Doruk Ozturk <doruk.ozturk@kitware.com>
2019-02-12 09:07:22 -0500

    Add a more complete metaboanalyst example

--- devops/examples/metaboanalyst.R
+++ devops/examples/metaboanalyst.R
@@ -1,7 +1,11 @@
 library("MetaboAnalystR");
 
-mSet <- InitDataObjects("conc", "stat", FALSE);
-mSet <- Read.TextData(mSet, "http://www.metaboanalyst.ca/resources/data/human_cachexia.csv", "rowu", "disc");
-mSet <- SanityCheckData(mSet);
-mSet <- ReplaceMin(mSet);
-saveRDS(mSet, file="data.Rda");
+mSet<-InitDataObjects("conc", "stat", FALSE);
+mSet<-Read.TextData(mSet, "./examples/data_original.csv", "rowu", "disc");
+mSet<-SanityCheckData(mSet);
+mSet<-RemoveMissingPercent(mSet, percent=0.5);
+mSet<-ImputeVar(mSet, method="min")
+mSet<-PreparePrenormData(mSet);
+mSet<-Normalization(mSet, "MedianNorm", "NULL", "MeanCenter", ratio=FALSE, ratioNum=20);
+mSet<-PlotNormSummary(mSet, "norm_0_", "png", 72, width=NA);
+mSet<-PlotSampleNormSummary(mSet, "snorm_0_", "png", 72, width=NA);
